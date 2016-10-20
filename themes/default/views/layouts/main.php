97c4e0972bc76583f6393c6f86b26185681107fc
Rana <saidurwd@gmail.com>
2016-10-20 22:40:14 +0600

    Piwik

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -21,6 +21,23 @@
             <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/html5shiv.js"></script>
             <script src="<?php echo Yii::app()->theme->baseUrl; ?>/js/respond.min.js"></script>
         <![endif]-->
+        <!-- Piwik -->
+        <script type="text/javascript">
+          var _paq = _paq || [];
+          _paq.push(["setCookieDomain", "*.www.goldenbusinessbd.com"]);
+          _paq.push(["setDomains", ["*.www.goldenbusinessbd.com"]]);
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//wa.optimosolution.com/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', '2']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <noscript><p><img src="//wa.optimosolution.com/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
+        <!-- End Piwik Code -->
     </head>
     <body>
         <div class="boxed animated fadeIn animation-delay-5">
